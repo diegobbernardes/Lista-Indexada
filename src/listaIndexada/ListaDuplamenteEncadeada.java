f6767839a927cecba1ab5d83630288cf8005215a
diegobbernardes <diegob.bernardes@gmail.com>
2016-11-30 11:23:49 -0200

    Implementação lista duplamente encadeada com skips

--- src/listaIndexada/ListaDuplamenteEncadeada.java
+++ src/listaIndexada/ListaDuplamenteEncadeada.java
@@ -6,6 +6,12 @@ public class ListaDuplamenteEncadeada<T>{
 		private Node next;
 		private Node previous;
 		
+		private Node skip25Next;
+		private Node skip25Previous;
+		
+		private Node skip100Next;
+		private Node skip100Previous;
+		
 		public Node(T value) {
 			data = value;
 		}		
@@ -14,10 +20,67 @@ public class ListaDuplamenteEncadeada<T>{
 		}
 	}
 	
+	public void mostraHead(){
+		System.out.println(head.skip25Next + " - " +head.skip25Previous + " - " + head );
+	}
+	public void mostraTail(){
+		System.out.println(tail.skip25Next + " - " +tail.skip25Previous + " - " + tail );
+	}
+	
+	public void listaCreateSkips(){
+		Node skip25 = head;
+		Node skip100 = head;
+		
+		Node current = head;
+		
+		skip25.skip25Next = tail;
+		skip100.skip100Next = tail;
+		
+		int cont = 1;
+		while(current != null){
+			if((cont % 25) == 0){
+				skip25.skip25Next = current;
+				current.skip25Previous = skip25;
+				current.skip25Next = tail;
+				
+				skip25 = current;
+			}
+			if((cont % 100) == 0){
+				skip100.skip100Next = current;
+				current.skip100Previous = skip100;
+				current.skip100Next = tail;
+				
+				skip100 = current;
+			}
+			current = current.next;
+			cont++;
+		}
+		if(tail.skip25Previous == null)
+			tail.skip25Previous = skip25;
+		if(tail.skip100Previous == null)
+			tail.skip100Previous = skip25;
+	}
+	
+	public void mostraListaInicioSkip25() {
+        Node current = head;
+        while (current != null) {
+            System.out.println((current.getData()));
+            current = current.skip100Next;
+        }
+    }
+	
+	public void mostraListaFimSkip25() {
+        Node current = tail;
+        while (current != null) {
+            System.out.println((current.getData()));
+            current = current.skip25Previous;
+        }
+    }
+	
 	public void mostraListaInicio() {
         Node current = head;
         while (current != null) {
-            System.out.println(((Pessoa) current.getData()).getNome());
+            System.out.println((current.getData()));
             current = current.next;
         }
     }
@@ -25,7 +88,7 @@ public class ListaDuplamenteEncadeada<T>{
 	public void mostraListaFim() {
         Node current = tail;
         while (current != null) {
-            System.out.println(((Pessoa) current.getData()).getNome());
+            System.out.println((current.getData()));
             current = current.previous;
         }
     }
--- src/listaIndexada/testeListaDuplamenteEncadeada.java
+++ src/listaIndexada/testeListaDuplamenteEncadeada.java
@@ -39,8 +39,20 @@ public class testeListaDuplamenteEncadeada {
 				lista.append(p);
 		} while (p != null);
 		
-		lista.mostraListaInicio();	
-		lista.mostraListaFim();	
+		ListaDuplamenteEncadeada<Integer> lista2 = new ListaDuplamenteEncadeada<>();
+		for (int i = 0; i <= 10000; i++) {
+			lista2.append(i);
+		}
+		
+		//lista.mostraListaInicio();	
+		//System.out.println("////\\\\");
+		//lista.mostraListaFim();	
+		lista2.listaCreateSkips();
+		lista2.mostraListaInicioSkip25();
+		lista2.mostraListaFimSkip25();
+		lista2.mostraHead();
+		lista2.mostraTail();
+		
 	}
 	
 	
